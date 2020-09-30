e8670714031076940a73b910a72650a4343e4037
Bechtel <ebechtel@contractor.usgs.gov>
2020-09-30 13:31:50 -0400

    ran lint

--- src/views/components/ImageCarousel.vue
+++ src/views/components/ImageCarousel.vue
@@ -1,106 +1,105 @@
 <template>
-    <carousel
-        id="image-slider"
-        :autoplay="true"
-        :autoplay-hover-pause="true"
-        :per-page="3"
-    >
-
-        <slide class="slide">
-          <div class="slider-image-container">
-              <picture>
-              <!-- <source srcset="@/assets/intro/images/imageSlider/slider_01.webp"
+  <carousel
+    id="image-slider"
+    :autoplay="true"
+    :autoplay-hover-pause="true"
+    :per-page="3"
+  >
+    <slide class="slide">
+      <div class="slider-image-container">
+        <picture>
+          <!-- <source srcset="@/assets/intro/images/imageSlider/slider_01.webp"
                       media="(max-width: 992px)"
                       type = "image/webp"  > -->
-              <source
-                  srcset="@/assets/images/fieldphotos/EmersonGulch.png" 
-                  media="(max-width: 992px)"
-                  type="image/jpeg"
-              >
-              <img
-                  src="@/assets/images/fieldphotos/EmersonGulch.png" 
-                  alt="A steep hilside burned by wildfire"
-              >
-              </picture>
-          </div>
-        </slide>
+          <source
+            srcset="@/assets/images/fieldphotos/EmersonGulch.png" 
+            media="(max-width: 992px)"
+            type="image/jpeg"
+          >
+          <img
+            src="@/assets/images/fieldphotos/EmersonGulch.png" 
+            alt="A steep hilside burned by wildfire"
+          >
+        </picture>
+      </div>
+    </slide>
 
-        <slide class="slide">
-          <div class="slider-image-container">
-              <picture>
-              <!-- <source srcset="@/assets/intro/images/imageSlider/slider_01.webp"
+    <slide class="slide">
+      <div class="slider-image-container">
+        <picture>
+          <!-- <source srcset="@/assets/intro/images/imageSlider/slider_01.webp"
                       media="(max-width: 992px)"
                       type = "image/webp"  > -->
-              <source
-                  srcset="@/assets/images/fieldphotos/runoff.png" 
-                  media="(max-width: 992px)"
-                  type="image/jpeg"
-              >
-              <img
-                  src="@/assets/images/fieldphotos/runoff.png" 
-                  alt="A steep hilside burned by wildfire"
-              >
-              </picture>
-          </div>
-        </slide>
+          <source
+            srcset="@/assets/images/fieldphotos/runoff.png" 
+            media="(max-width: 992px)"
+            type="image/jpeg"
+          >
+          <img
+            src="@/assets/images/fieldphotos/runoff.png" 
+            alt="A steep hilside burned by wildfire"
+          >
+        </picture>
+      </div>
+    </slide>
 
-        <slide class="slide">
-          <div class="slider-image-container">
-              <picture>
-              <!-- <source srcset="@/assets/intro/images/imageSlider/slider_01.webp"
+    <slide class="slide">
+      <div class="slider-image-container">
+        <picture>
+          <!-- <source srcset="@/assets/intro/images/imageSlider/slider_01.webp"
                       media="(max-width: 992px)"
                       type = "image/webp"  > -->
-              <source
-                  srcset="@/assets/images/fieldphotos/ColdSprings_BrianDebSheila.jpg" 
-                  media="(max-width: 992px)"
-                  type="image/jpeg"
-              >
-              <img
-                  src="@/assets/images/fieldphotos/ColdSprings_BrianDebSheila.jpg" 
-                  alt="Sheila, Brian, and Deb in the field"
-              >
-              </picture>
-          </div>
-        </slide>
+          <source
+            srcset="@/assets/images/fieldphotos/ColdSprings_BrianDebSheila.jpg" 
+            media="(max-width: 992px)"
+            type="image/jpeg"
+          >
+          <img
+            src="@/assets/images/fieldphotos/ColdSprings_BrianDebSheila.jpg" 
+            alt="Sheila, Brian, and Deb in the field"
+          >
+        </picture>
+      </div>
+    </slide>
 
-        <slide class="slide">
-          <div class="slider-image-container">
-              <picture>
-              <!-- <source srcset="@/assets/intro/images/imageSlider/slider_01.webp"
+    <slide class="slide">
+      <div class="slider-image-container">
+        <picture>
+          <!-- <source srcset="@/assets/intro/images/imageSlider/slider_01.webp"
                       media="(max-width: 992px)"
                       type = "image/webp"  > -->
-              <source
-                  srcset="@/assets/images/fieldphotos/runoff2.jpg" 
-                  media="(max-width: 992px)"
-                  type="image/jpeg"
-              >
-              <img
-                  src="@/assets/images/fieldphotos/runoff2.jpg" 
-                  alt="A steep hilside burned by wildfire"
-              >
-              </picture>
-          </div>
-        </slide>
+          <source
+            srcset="@/assets/images/fieldphotos/runoff2.jpg" 
+            media="(max-width: 992px)"
+            type="image/jpeg"
+          >
+          <img
+            src="@/assets/images/fieldphotos/runoff2.jpg" 
+            alt="A steep hilside burned by wildfire"
+          >
+        </picture>
+      </div>
+    </slide>
 
-        <slide class="slide">
-          <div class="slider-image-container">
-              <picture>
-              <!-- <source srcset="@/assets/intro/images/imageSlider/slider_01.webp"
+    <slide class="slide">
+      <div class="slider-image-container">
+        <picture>
+          <!-- <source srcset="@/assets/intro/images/imageSlider/slider_01.webp"
                       media="(max-width: 992px)"
                       type = "image/webp"  > -->
-              <source
-                  srcset="@/assets/images/fieldphotos/SoilSampleSite2.jpg" 
-                  media="(max-width: 992px)"
-                  type="image/jpeg"
-              >
-              <img
-                  src="@/assets/images/fieldphotos/SoilSampleSite2.jpg" 
-                  alt="A steep hilside burned by wildfire"
-              >
-              </picture>
-          </div>
-        </slide>
-    </carousel>
+          <source
+            srcset="@/assets/images/fieldphotos/SoilSampleSite2.jpg" 
+            media="(max-width: 992px)"
+            type="image/jpeg"
+          >
+          <img
+            src="@/assets/images/fieldphotos/SoilSampleSite2.jpg" 
+            alt="A steep hilside burned by wildfire"
+          >
+        </picture>
+      </div>
+    </slide>
+  </carousel>
 </template>
 
 <script>

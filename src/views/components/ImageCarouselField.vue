fa1c19b916feb81e32cbd4779e18793b582befa4
coll nell <collnellphd@gmail.com>
2020-10-14 17:21:05 -0500

    adding compressed images with  lazy load

--- src/views/components/ImageCarouselField.vue
+++ src/views/components/ImageCarouselField.vue
@@ -8,15 +8,23 @@
     <slide class="slide">
       <div class="slider-image-container">
         <picture>
+
           <source
-            srcset="@/assets/images/fieldphotos/Soil_sample_site_5_pic4.png" 
-            media="(max-width: 992px)"
+            srcset="@/assets/images/fieldphotos/Soil_sample_site_5_pic4_800w.jpg 800w,
+            @/assets/images/fieldphotos/Soil_sample_site_5_pic4_400w.jpg 400w" 
+            media="(max-width: 800px)"
+            type="image/jpeg"
+          >
+          <source
+            srcset="@/assets/images/fieldphotos/Soil_sample_site_5_pic4_2200w.jpg" 
+            media="(min-width: 801px)"
             type="image/jpeg"
           >
           <img
             v-img:group-2
-            src="@/assets/images/fieldphotos/Soil_sample_site_5_pic4.png" 
+            src="@/assets/images/fieldphotos/Soil_sample_site_5_pic4_2200w.jpg" 
             alt="USGS Soil Sample Site"
+            loading = "lazy"
           >
         </picture>
       </div>
@@ -26,14 +34,21 @@
       <div class="slider-image-container">
         <picture>
           <source
-            srcset="@/assets/images/fieldphotos/Brian_sampling_ai.jpg" 
-            media="(max-width: 992px)"
+            srcset="@/assets/images/fieldphotos/Brian_sampling_ai_800w.webp" 
+            media="(max-width: 800px)"
             type="image/jpeg"
           >
+          <source
+            srcset="@/assets/images/fieldphotos/Brian_sampling_ai_800w.jpg" 
+            media="(max-width: 800px)"
+            type="image/jpeg"
+          >
+          
           <img
             v-img:group-2
-            src="@/assets/images/fieldphotos/Brian_sampling_ai.jpg" 
+            src="@/assets/images/fieldphotos/Brian_sampling_ai_2500w.jpg" 
             alt="USGS Hydrologist Brian Ebel sampling soil"
+            loading = "lazy"
           >
         </picture>
       </div>
@@ -45,14 +60,30 @@
       <div class="slider-image-container">
         <picture>
           <source
-            srcset="@/assets/images/fieldphotos/2Aug2011_incipient_runoff.png" 
+            srcset="@/assets/images/fieldphotos/2Aug2011_incipient_runoff_400w.webp 400w" 
             media="(max-width: 992px)"
+            type="image/webp"
+          >
+           <source
+            srcset="@/assets/images/fieldphotos/2Aug2011_incipient_runoff_2200w.webp 2200w" 
+            media="(min-width: 993px)"
+            type="image/webp"
+          >
+          <source
+            srcset="@/assets/images/fieldphotos/2Aug2011_incipient_runoff_400w.jpg 400w" 
+            media="(max-width: 992px)"
+            type="image/jpeg"
+          >
+           <source
+            srcset="@/assets/images/fieldphotos/2Aug2011_incipient_runoff_2200w.jpg 2200w" 
+            media="(min-width: 993px)"
             type="image/jpeg"
           >
           <img
             v-img:group-2
-            src="@/assets/images/fieldphotos/2Aug2011_incipient_runoff.png" 
+            src="@/assets/images/fieldphotos/2Aug2011_incipient_runoff_2200w.jpg" 
             alt="A burned soil sample site"
+            loading = "lazy"
           >
         </picture>
       </div>
@@ -62,14 +93,31 @@
       <div class="slider-image-container">
         <picture>
           <source
-            srcset="@/assets/images/fieldphotos/ColdSprings_BrianDebSheila_ai.jpg"
+            srcset="@/assets/images/fieldphotos/ColdSprings_BrianDebSheila_ai_400.webp 400w"
             media="(max-width: 992px)"
+            type="image/webp"
+          >
+          <source
+            srcset="@/assets/images/fieldphotos/ColdSprings_BrianDebSheila_ai_800.webp 800w"
+            media="(max-width: 800px)"
+            type="image/webp"
+          >
+          
+          <source
+            srcset="@/assets/images/fieldphotos/ColdSprings_BrianDebSheila_ai_400.jpg 400w"
+            media="(max-width: 992px)"
+            type="image/jpeg"
+          >
+          <source
+            srcset="@/assets/images/fieldphotos/ColdSprings_BrianDebSheila_ai_800.jpg 800w"
+            media="(max-width: 800px)"
             type="image/jpeg"
           >
           <img
             v-img:group-2
-            src="@/assets/images/fieldphotos/ColdSprings_BrianDebSheila_ai.jpg" 
+            src="@/assets/images/fieldphotos/ColdSprings_BrianDebSheila_ai_2200.jpg" 
             alt="USGS Hydrologists Sheila Murphy, Deb, and Brian Ebel"
+            loading = "lazy"
           >
         </picture>
       </div>
@@ -79,14 +127,20 @@
       <div class="slider-image-container">
         <picture>
           <source
-            srcset="@/assets/images/fieldphotos/IMG_0620_labels.jpg" 
-            media="(max-width: 992px)"
+            srcset="@/assets/images/fieldphotos/IMG_0620_labels_800w.jpg 800w" 
+            media="(max-width: 800px)"
+            type="image/jpeg"
+          >
+          <source
+            srcset="@/assets/images/fieldphotos/IMG_0620_labels_2200w.jpg" 
+            media="(min-width: 801px)"
             type="image/jpeg"
           >
           <img
             v-img:group-2
-            src="@/assets/images/fieldphotos/IMG_0620_labels.jpg" 
+            src="@/assets/images/fieldphotos/IMG_0620_labels_2200w.jpg" 
             alt="A burned soil sample site"
+            loading = "lazy"
           >
         </picture>
       </div>
@@ -96,14 +150,21 @@
       <div class="slider-image-container">
         <picture>
           <source
-            srcset="@/assets/images/fieldphotos/DSCN0082.png" 
+            srcset="@/assets/images/fieldphotos/DSCN0082_400w.jpg 400w,
+            @/assets/images/fieldphotos/DSCN0082_800w.jpg 800w" 
             media="(max-width: 992px)"
+            type="image/jpeg"
+          >
+          <source
+            srcset="@/assets/images/fieldphotos/DSCN0082_2200w.jpg" 
+            media="(min-width: 993px)"
             type="image/jpeg"
           >
           <img
             v-img:group-2
-            src="@/assets/images/fieldphotos/DSCN0082.png" 
+            src="@/assets/images/fieldphotos/DSCN0082_2200w.jpg" 
             alt="A burned soil sample site"
+            loading = "lazy"
           >
         </picture>
       </div>
